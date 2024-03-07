78294081473ff2e28df8c93f25b8c9dd7ed0766d
Jakub <42150609+JacquesM27@users.noreply.github.com>
2024-03-07 19:21:52 +0100

    Create RestaurantMapping.cs

---
+++ src/Modules/Trips/PathPilot.Modules.Trips.Application/Restaurants/Mapping/RestaurantMapping.cs
@@ -0,0 +1,26 @@
+﻿using PathPilot.Modules.Trips.Application.Restaurants.DTO;
+using PathPilot.Modules.Trips.Domain.Restaurants.Entities;
+using PathPilot.Modules.Trips.Domain.Restaurants.ValueObjects;
+using PathPilot.Modules.Trips.Domain.ValueObjects;
+
+namespace PathPilot.Modules.Trips.Application.Restaurants.Mapping;
+
+internal static class RestaurantMapping
+{
+    internal static RestaurantDto ToDto(this Restaurant restaurant)
+        => new(restaurant.Id,
+            restaurant.Name,
+            restaurant.Description,
+            restaurant.IsOpened, restaurant.AverageRate, restaurant.CuisineType,
+            restaurant.Address?.ToDto(),
+            restaurant.MenuItems.Select(x => x.ToDto())); 
+
+    internal static MenuItemDto ToDto(this MenuItem menuItem)
+        => new(menuItem.Name, menuItem.Description, menuItem.Price);
+
+    internal static AddressDto? ToDto(this Address? address)
+        => address is null 
+            ? null 
+            : new AddressDto(address.City, address.Street, address.BuildingNumber, address.PostCode,
+            address.Country, address.Longitude, address.Latitude);
+}
