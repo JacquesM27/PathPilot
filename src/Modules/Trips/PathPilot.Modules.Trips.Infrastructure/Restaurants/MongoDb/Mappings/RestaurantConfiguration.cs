00ccacb120c2c598b03c61cfc38821e573592d09
Jakub <42150609+JacquesM27@users.noreply.github.com>
2024-03-08 19:44:10 +0100

    created restaurant repository and entity configuration for mongo

---
+++ src/Modules/Trips/PathPilot.Modules.Trips.Infrastructure/Restaurants/MongoDb/Mappings/RestaurantConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using PathPilot.Modules.Trips.Domain.Restaurants.Entities;
+
+namespace PathPilot.Modules.Trips.Infrastructure.Restaurants.MongoDb.Mappings;
+
+internal static class RestaurantConfiguration
+{
+    internal const string CollectionName = "Restaurants";
+    
+    internal static void ConfigureEntity()
+    {
+        BsonClassMap.RegisterClassMap<Restaurant>(cm =>
+        {
+            cm.AutoMap();
+            cm.MapIdMember(c => c.Id).SetIdGenerator(StringObjectIdGenerator.Instance);
+        });
+    }
+}
---
+++ src/Modules/Trips/PathPilot.Modules.Trips.Infrastructure/Restaurants/MongoDb/Repositories/RestaurantRepository.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using PathPilot.Modules.Trips.Domain.Restaurants.Entities;
+using PathPilot.Modules.Trips.Domain.Restaurants.Repositories;
+using PathPilot.Shared.Abstractions.Kernel.Types;
+
+namespace PathPilot.Modules.Trips.Infrastructure.Restaurants.MongoDb.Repositories;
+
+internal sealed class RestaurantRepository(
+    IMongoCollection<Restaurant> collection
+    ) : IRestaurantRepository
+{
+    private readonly FilterDefinitionBuilder<Restaurant> _filterBuilder = Builders<Restaurant>.Filter;
+
+    public async Task<IEnumerable<Restaurant>> BrowseAsync() //TODO: add pagination 
+        => await collection.Find(_filterBuilder.Empty).ToListAsync();
+
+    public async Task<IEnumerable<Restaurant>> BrowseAsync(IEnumerable<EntityId> ids)
+    {
+        var filter = _filterBuilder.In(r => r.Id, ids);
+        return await collection.Find(filter).ToListAsync();
+    }
+
+    public async Task<Restaurant> GetAsync(EntityId id)
+    {
+        var filter = _filterBuilder.Eq(r => r.Id, id);
+        return await collection.Find(filter).FirstOrDefaultAsync();
+    }
+
+    public async Task AddAsync(Restaurant restaurant)
+    {
+        await collection.InsertOneAsync(restaurant);
+    }
+
+    public async Task UpdateAsync(Restaurant restaurant)
+    {
+        var filter = _filterBuilder.Eq(r => r.Id, restaurant.Id);
+        await collection.ReplaceOneAsync(filter, restaurant);
+    }
+}
